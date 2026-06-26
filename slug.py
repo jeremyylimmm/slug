296e12fbd214cd24b8af583b5b3b8f790cc5634f
jeremyylimmm <jeremylim.0304@gmail.com>
2026-06-27 02:40:19 +1000

    Remove python perft

--- slug.py
+++ slug.py
@@ -5,22 +5,6 @@ from slime import Position
 STARTPOS_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 KIWIPETE_FEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
-def perft(pos, depth):
-    if depth <= 0:
-        return 1
-
-    side = pos.stm()
-
-    count = 0
-
-    for mv in pos.gen_pseudolegal_moves():
-        child = pos.make_move(mv)
-        if child.checked(side):
-            continue
-        count += perft(child, depth-1)
-
-    return count
-
 def splitperft(pos, depth):
     side = pos.stm()
 
@@ -30,23 +14,23 @@ def splitperft(pos, depth):
         child = pos.make_move(mv)
         if child.checked(side):
             continue
-        result = perft(child, depth-1)
+        result = child.perft(depth-1)
         print(f"{pos.to_uci_move(mv)}: {result}")
         total += result
 
     print(f"Total: {total}")
 
+    return total
+
 pos = Position(KIWIPETE_FEN)
 
+perft_depth = 5
 
 start_time = time.perf_counter()
-result = pos.perft(6)
+result = splitperft(pos, perft_depth)
 end_time = time.perf_counter()
 
 elapsed = end_time - start_time
 nps = result / elapsed
 
-print(f"Perft(5) - Kiwipete: {result} ({nps/1_000_000:.6}M nps)")
-
-#pos = pos.make_move(pos.from_uci_move("e5d7"))
-#splitperft(pos, 1)
+print(f"Perft({perft_depth}) - Kiwipete: {result} ({nps/1_000_000:.6}M nps)")
